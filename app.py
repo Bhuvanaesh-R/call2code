5dc5f9b5e2c064a3f7d9003f2bb9a98c7036fb17
Bhuvanaesh R <bhuvanaesh11@gmail.com>
2025-07-05 05:48:16 +0530

    added login frontend design

--- app.py
+++ app.py
@@ -22,10 +22,16 @@ def after_request(response):
     return response
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+
+    return render_template("login.html")
 
 
 if __name__ == '__main__':  
