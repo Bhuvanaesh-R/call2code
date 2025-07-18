5995209d54cedc8ae8d08757000aa55c1f8c7ed6
Bhuvanaesh R <bhuvanaesh11@gmail.com>
2025-07-18 21:44:08 +0530

    Solved major bugs in category and channel creation

--- static/script.js
+++ static/script.js
@@ -41,17 +41,16 @@ btnChannelName = document.querySelector('#btn-channel-name')
 channelname = document.querySelector('#channelname')
 channels = null;
 currentChannel = null;
+permissionsList = document.querySelector('#permission-list');
 
 
 btnRevealCategorylList.addEventListener('click', function() {
-    withinCategoryLimit = noofcategory.value > 1 && noofcategory.value <= categoryLimit;
+    withinCategoryLimit = noofcategory.value >= 1 && noofcategory.value <= categoryLimit;
 
 
     if (withinCategoryLimit) {
-        btnRevealCategorylList.classList.add('disabled');
-
-        for (let i = 1; i <= noofcategory.value; i++) {
-            serverData.push({"name":"Category " + i, "channels":[]})
+        for (let i = 0, num = serverData.length + 1; i < noofcategory.value; num++, i++) {
+            serverData.push({"name":"Category " + num, "channels":[]})
         };
         
         displayCategory();
@@ -68,15 +67,14 @@ btnRevealCategorylList.addEventListener('click', function() {
 
 
 btnRevealChannelList.addEventListener('click', function() {
-    channelCount += noofchannel.value;
-    withinChannelLimit = channelCount <= channelLimit;
+    withinChannelLimit = channelCount + parseInt(noofchannel.value) <= channelLimit;
 
     if (noofchannel != '' && withinChannelLimit) {
-        btnRevealChannelList.classList.add('disabled');
+        channelCount += parseInt(noofchannel.value);
         labelNoofchannel.innerHTML = `No of Channel to be added (${channelCount} out of 500 Used): `;
 
-        for (let i = 1; i <= noofchannel.value; i++) {
-            serverData[currentCategory.slice(-2) - 1].channels.push({"name":"Channel " + i});
+        for (let i = 0, num = serverData[currentCategory.slice(-2) - 1]['channels'].length + 1; i < noofchannel.value; num++, i++) {
+            serverData[currentCategory.slice(-2) - 1].channels.push({"name":"Channel " + num});
         };
 
         displayChannel()
@@ -95,14 +93,21 @@ btnRevealChannelList.addEventListener('click', function() {
 function selectCategory() {
     categories.forEach(category => {
         category.addEventListener('click', function() {
+            channelList.innerHTML = 'None';
             currentCategory = category.id;
             clearSelectedCategory();
             category.classList.remove('btn-outline-secondary');
             category.classList.add('btn-success', 'disabled');
-            btnCategoryName.classList.remove('disabled');
 
 
             labelNoofchannel.innerHTML = `No of Channel to be added (${channelCount} out of 500 Used): `;
+
+            displayChannel();
+
+            channels = document.querySelectorAll('.channels');
+
+            selectChannel();
+            changeChannelName();
         });
     });
 };
@@ -111,8 +116,6 @@ function selectCategory() {
 function changeCategoryName() {
     btnCategoryName.addEventListener('click', function() {
         if (categoryname.value != '') {
-            btnCategoryName.classList.add('disabled');
-            btnRevealChannelList.classList.remove('disabled');
             serverData[currentCategory.slice(-2) - 1].name = categoryname.value;
             document.querySelector(`#${currentCategory}`).innerHTML = categoryname.value;
         };
@@ -150,7 +153,7 @@ function clearSelectedChannel() {
 
 function displayChannel() {
     channelList.innerHTML = '';
-    for (let i = 0; i < noofchannel.value; i++) {
+    for (let i = 0; i < serverData[currentCategory.slice(-2) - 1]['channels'].length; i++) {
         id = i >= 9 ? i + 1 : '0' + (i + 1);
         channelList.innerHTML += `<span class="channels btn btn-outline-secondary" id="channel-${id}">${serverData[currentCategory.slice(-2) - 1]['channels'][i]['name']}</span>`;
     };
@@ -174,8 +177,13 @@ function changeChannelName() {
     btnChannelName.addEventListener('click', function() {
         if (channelname.value != '') {
             btnChannelName.classList.add('disabled');
-            serverData[currentCategory.slice(-2) - 1]['channels'][currentChannel.slice(-2) - 1]['name'].name = channelname.value;
+            serverData[currentCategory.slice(-2) - 1]['channels'][currentChannel.slice(-2) - 1]['name']= channelname.value;
             document.querySelector(`#${currentChannel}`).innerHTML = channelname.value;
         };
     });
 };
+
+/* 
+function displayPermissions() {
+    permissionsList.
+} */
